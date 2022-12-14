a3f98f7084c1207623f1e058e029fd4f01206a5c
Omer Shtivi <omer.shtivi@satoricyber.com>
2022-12-14 17:37:40 +0200

    BigQuery add support for creditinals as string

--- authz_analyzer/__init__.py
+++ authz_analyzer/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """SatoriCyber"""
 __email__ = 'omer.shtivi@satoricyber.com'
-__version__ = '2.4.0'
+__version__ = '2.4.1'
 
 from authz_analyzer.datastores.bigquery.analyzer import BigQueryAuthzAnalyzer
 from authz_analyzer.datastores.postgres.analyzer import PostgresAuthzAnalyzer
