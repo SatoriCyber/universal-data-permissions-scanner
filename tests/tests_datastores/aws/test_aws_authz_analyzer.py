967c3918ce28726d849d518c53682a7fc9c08aa4
Alon <alon@satoricyber.com>
2022-12-21 09:28:52 +0200

    SGA-5958 runtime s3 support - ongoing

--- tests/tests_datastores/aws/test_aws_authz_analyzer.py
+++ tests/tests_datastores/aws/test_aws_authz_analyzer.py
@@ -3,15 +3,30 @@ import pytest
 import pathlib
 import json
 from authz_analyzer.datastores.aws.aws_authz_analyzer import AwsAuthzAnalyzer
-from authz_analyzer.datastores.aws.services.s3.s3_service import S3ServiceType
+from authz_analyzer.datastores.aws.services.s3.s3_service import S3ServiceType, S3_SERVICE_NAME
+from authz_analyzer.datastores.aws.services.s3.s3_actions import S3Action
+from authz_analyzer.datastores.aws.services.s3.bucket import S3Bucket
+from authz_analyzer.datastores.aws.services.service_base import (
+    register_service_action_by_name,
+    register_service_resource_by_name,
+    register_service_type_by_name,
+)
 from authz_analyzer.datastores.aws.utils.create_session import create_session_with_assume_role
 from authz_analyzer.utils.logger import get_logger
 from serde.json import to_json, from_dict
+from tests.mocks.mock_writers import MockWriter
 
 
 AWS_AUTHZ_ANALYZER_SATORI_DEV_JSON_FILE = pathlib.Path().joinpath(
     os.path.dirname(__file__), 'satori_dev_account_authz_analyzer.json'
 )
+
+
+@pytest.fixture
+def register_services_for_deserialize_from_file(tmpdir):
+    register_service_type_by_name(S3_SERVICE_NAME, S3ServiceType)
+    register_service_action_by_name(S3_SERVICE_NAME, S3Action)
+    register_service_resource_by_name(S3_SERVICE_NAME, S3Bucket)
 
 
 @pytest.mark.skipif(
@@ -33,10 +48,22 @@ def test_aws_authz_analyzer_with_s3_write_satori_dev_account():
     not os.environ.get("AUTHZ_SATORI_DEV_ACCOUNT_TEST"),
     reason="not really a test, just pull latest satori dev account config and write it to file",
 )
-def test_authz_analyzer_load_satori_dev_json_file():
+def test_aws_authz_analyzer_load_satori_dev_json_file(register_services_for_deserialize_from_file):
     with open(AWS_AUTHZ_ANALYZER_SATORI_DEV_JSON_FILE, "r") as f:
         authz_analyzer_json_from_file = json.load(f)
         authz_analyzer = from_dict(AwsAuthzAnalyzer, authz_analyzer_json_from_file)
         authz_analyzer_json_from_serde = json.loads(to_json(authz_analyzer))
 
         assert authz_analyzer_json_from_file == authz_analyzer_json_from_serde
+
+
+@pytest.mark.skipif(
+    not os.environ.get("AUTHZ_SATORI_DEV_ACCOUNT_TEST"),
+    reason="not really a test, just pull latest satori dev account config and write it to file",
+)
+def test_aws_authz_analyzer_analyzed_permissions_satori_dev_json_file(register_services_for_deserialize_from_file):
+    with open(AWS_AUTHZ_ANALYZER_SATORI_DEV_JSON_FILE, "r") as f:
+        authz_analyzer_json_from_file = json.load(f)
+        authz_analyzer: AwsAuthzAnalyzer = from_dict(AwsAuthzAnalyzer, authz_analyzer_json_from_file)
+        mocked_writer = MockWriter.get()
+        authz_analyzer.write_permissions(get_logger(False), mocked_writer.mocked_writer)
