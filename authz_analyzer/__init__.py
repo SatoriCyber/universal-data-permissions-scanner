06bc0d8e010c83bc20fde7a52c89e6c97edb4ffb
Alon <alon@satoricyber.com>
2022-12-25 11:13:44 +0200

    SGA-5958 bump version to support the s3 datastore, single account

--- authz_analyzer/__init__.py
+++ authz_analyzer/__init__.py
@@ -2,9 +2,10 @@
 
 __author__ = """SatoriCyber"""
 __email__ = 'omer.shtivi@satoricyber.com'
-__version__ = '3.1.0'
+__version__ = '3.2.0'
 
 from authz_analyzer.datastores.bigquery.analyzer import BigQueryAuthzAnalyzer  # type: ignore
 from authz_analyzer.datastores.postgres.analyzer import PostgresAuthzAnalyzer  # type: ignore
 from authz_analyzer.datastores.redshift.analyzer import RedshiftAuthzAnalyzer  # type: ignore
 from authz_analyzer.datastores.snowflake.analyzer import SnowflakeAuthzAnalyzer  # type: ignore
+from authz_analyzer.datastores.aws.services.s3.analyzer import S3AuthzAnalyzer  # type: ignore
