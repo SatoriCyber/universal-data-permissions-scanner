67d51dc280d323089d6f9061a4203343d7c2b67b
alonlud <alon@satoricyber.com>
2022-12-25 17:27:12 +0200

    SGA-5958 ongoing s3 (#63)

--- authz_analyzer/__init__.py
+++ authz_analyzer/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """SatoriCyber"""
 __email__ = 'omer.shtivi@satoricyber.com'
-__version__ = '3.2.2'
+__version__ = '3.2.3'
 
 from authz_analyzer.datastores.bigquery.analyzer import BigQueryAuthzAnalyzer  # type: ignore
 from authz_analyzer.datastores.postgres.analyzer import PostgresAuthzAnalyzer  # type: ignore
