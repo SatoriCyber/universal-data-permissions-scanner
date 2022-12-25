28b94d49be9aac97e495239ba7b9d1d87e5e97d5
Omer Shtivi <omer.shtivi@satoricyber.com>
2022-12-25 15:05:33 +0200

    support for public role Snowflake

--- authz_analyzer/datastores/snowflake/commands/user_grants.sql
+++ authz_analyzer/datastores/snowflake/commands/user_grants.sql
@@ -10,16 +10,18 @@ with users_grants as (
 users as (
     SELECT
         NAME as user,
-        email
+        email,
+        default_role
     from
         snowflake.account_usage.users
 )
 select
-    users_grants.user as user,
+    users.user as user,
     users_grants.role as role,
-    users.email as email
+    users.email as email,
+    users.default_role
 from
     users_grants
-    inner join users on users_grants.user = users.user
+    right outer join users on users_grants.user = users.user
 order by
     users_grants.user
