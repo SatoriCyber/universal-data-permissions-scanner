1ab1bec1b171fb3d92f783604d280eeac5907f8d
Omer Shtivi <omer.shtivi@satoricyber.com>
2023-02-06 18:13:02 +0200

    Snowflake aggregate permissions

--- authz_analyzer/datastores/snowflake/commands/grants_roles.sql
+++ authz_analyzer/datastores/snowflake/commands/grants_roles.sql
@@ -1,10 +1,9 @@
 SELECT
-    NAME as name,
     GRANTEE_NAME as grantee_name,
     PRIVILEGE as privilege,
     TABLE_CATALOG as db,
     TABLE_SCHEMA as schema,
-    name as table_name,
+    name as resource_name,
     GRANTED_ON as granted_on
 FROM
     snowflake.account_usage.grants_to_roles
@@ -12,4 +11,7 @@ where
     deleted_on is null
     and GRANTED_ON in ('TABLE', 'VIEW', 'MATERIALIZED VIEW', 'ROLE')
 order by
-    table_name
+    grantee_name,
+    db,
+    schema,
+    resource_name
