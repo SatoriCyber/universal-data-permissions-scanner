f45d716d2af2909968797b297d0d98875d4e2e0d
Alon <alon@satoricyber.com>
2023-02-07 11:08:29 +0200

    SGA-6144 support external id (in assume role) for AWS s3

--- authz_analyzer/cli.py
+++ authz_analyzer/cli.py
@@ -2,7 +2,7 @@
 import os
 import sys
 from pathlib import Path
-from typing import Optional, List
+from typing import List, Optional
 
 import click
 
--- authz_analyzer/datastores/aws/aws_ptrp_package/aws_ptrp/utils/create_session.py
+++ authz_analyzer/datastores/aws/aws_ptrp_package/aws_ptrp/utils/create_session.py
@@ -1,4 +1,5 @@
 from typing import Optional
+
 import boto3
 from boto3 import Session
 
