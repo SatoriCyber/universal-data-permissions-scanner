9763024d70a24e0326a0c5735e4c8c574feae630
omer-shtivi <85064354+omer-shtivi@users.noreply.github.com>
2023-03-23 09:07:43 +0200

    Bump version: 0.1.9 → 0.1.10 (#200)

--- authz_analyzer/__init__.py
+++ authz_analyzer/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """SatoriCyber"""
 __email__ = 'contact@satoricyber.com'
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 
 from authz_analyzer.datastores.aws.analyzer import AwsAssumeRoleInput, AWSAuthzAnalyzer  # type: ignore
 from authz_analyzer.datastores.aws.analyzer.redshift.analyzer import RedshiftAuthzAnalyzer  # type: ignore
