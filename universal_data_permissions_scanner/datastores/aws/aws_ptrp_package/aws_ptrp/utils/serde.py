6d04182ec21d499f8ab4981ed2a5a78ac2535274
Omer Shtivi <omer.shtivi@satoricyber.com>
2023-11-13 10:15:43 +0200

    do any for serde enum field

--- universal_data_permissions_scanner/datastores/aws/aws_ptrp_package/aws_ptrp/utils/serde.py
+++ universal_data_permissions_scanner/datastores/aws/aws_ptrp_package/aws_ptrp/utils/serde.py
@@ -1,7 +1,7 @@
-from typing import Type
+from typing import Any
 
 from serde import field
 
 
-def serde_enum_field(enum_type: Type):
+def serde_enum_field(enum_type: Any) -> Any:
     return field(serializer=lambda x: x.name, deserializer=lambda x: enum_type[x])
