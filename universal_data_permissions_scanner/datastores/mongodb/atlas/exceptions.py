98c5c1dad3db5b780774c8c105b25cd25a73987f
omer-shtivi <85064354+omer-shtivi@users.noreply.github.com>
2023-12-05 15:16:58 +0200

    Custom error on resource collection (#265)

--- universal_data_permissions_scanner/datastores/mongodb/atlas/exceptions.py
+++ universal_data_permissions_scanner/datastores/mongodb/atlas/exceptions.py
@@ -1,7 +1,15 @@
 from universal_data_permissions_scanner.datastores.mongodb.atlas.service_model import ActionEntry
 
+from universal_data_permissions_scanner.datastores.mongodb.service_model import ResourceEntry
+
 
 class ActionResourcesNotFoundException(Exception):
     def __init__(self, action: ActionEntry) -> None:
         self.action = action
         super().__init__(f"Resources not found for action: {self.action}")
+
+
+class ActionResourceNotFoundException(Exception):
+    def __init__(self, resource: ResourceEntry) -> None:
+        self.resource = resource
+        super().__init__(f"Collection not found for resource: {self.resource}")
