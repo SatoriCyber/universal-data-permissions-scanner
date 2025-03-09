172ae0fdd1f17eda1f4e5836f8ecb10d63e2e23a
AvivDavid-Satori <107786696+AvivDavid-Satori@users.noreply.github.com>
2025-03-09 13:04:57 +0200

    bump version (#295)
    * Bump version: 0.1.36 → 0.1.37
    
    * Bump version: 0.1.37 → 0.1.38

--- universal_data_permissions_scanner/__init__.py
+++ universal_data_permissions_scanner/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """SatoriCyber"""
 __email__ = 'contact@satoricyber.com'
-__version__ = '0.1.36'
+__version__ = '0.1.38'
 
 from universal_data_permissions_scanner.datastores.aws.analyzer import AwsAssumeRoleInput, AWSAuthzAnalyzer  # type: ignore
 from universal_data_permissions_scanner.datastores.aws.analyzer.redshift.analyzer import RedshiftAuthzAnalyzer  # type: ignore
